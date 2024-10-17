83cc9dc5a3c6d706f4c13f1f567abafb7e58261c
faizaazam-1 <faizaazam.nu@gmail.com>
2024-10-17 19:27:39 +0500

    created a profile.jsx "/profile" to test navigation

--- app/index.jsx
+++ app/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
@@ -6,6 +7,9 @@ export default function App() {
     <View style={styles.container}>
       <Text>MY-APP ---- AORA</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "pink" }}>
+        Go to Profile
+      </Link>
     </View>
   );
 }
