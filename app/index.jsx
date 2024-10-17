30f4a493c41340bc8b6f37ff77076f11708528d3
faizaazam-1 <faizaazam.nu@gmail.com>
2024-10-17 23:58:29 +0500

    updated tailwind.config.js to add fonts and colors

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,11 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="test-3xl">MY-APP ---- AORA</Text>
+      <Text className="text-4xl">MY-APP ---- AORA</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "pink" }}>
         Go to Profile
