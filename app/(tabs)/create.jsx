3eb32a77bb3f1de736a2bbf189fc7c6a155fcfb2
faizaazam-1 <faizaazam.nu@gmail.com>
2024-10-24 09:42:48 +0500

    set up file and folder structure

---
+++ app/(tabs)/create.jsx
@@ -0,0 +1,13 @@
+//create and add videos to our platform
+import { View, Text } from "react-native";
+import React from "react";
+
+const Create = () => {
+  return (
+    <View>
+      <Text>Create</Text>
+    </View>
+  );
+};
+
+export default Create;
---
+++ app/search/[query].jsx
@@ -0,0 +1,14 @@
+//get the query information fot the specific search our user is trying to do.
+
+import { View, Text } from "react-native";
+import React from "react";
+
+const Search = () => {
+  return (
+    <View>
+      <Text>Search</Text>
+    </View>
+  );
+};
+
+export default Search;
